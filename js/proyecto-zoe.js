a0d29b8135ad16c67aab01478b0c99927832fe6e
proyectozoe <116409905+proyectozoe@users.noreply.github.com>
2022-10-22 22:43:58 -0300

    Add files via upload

---
+++ js/proyecto-zoe.js
@@ -0,0 +1,38 @@
+window.addEventListener("load",function(){
+    
+    //efecto botones cabecera
+    /*var botones = document.getElementsByClassName("efecto1");
+    console.log(botones);
+    for(var boton in botones){
+        if(botones[boton].className == "efecto1"){
+            botones[boton].addEventListener("mouseover",function(){
+                this.style.fontSize = "25px";
+            });
+            
+            if(botones[boton]. className == "efecto1"){
+                botones[boton].addEventListener("mouseout",function(){
+                    this.style.fontSize = "15px"
+                });
+            };
+        };   
+    }*/
+   //----------------------------------------------------------------------
+});
+//efectos sube y baja botonos cabecera
+
+/*$(document).ready(function(){
+    var menu = $('#lista');
+    menu.animate({
+        marginTop: '-180px',
+    },'slow')
+        .animate({
+            marginTop: '180px',
+        },'slow')
+        .animate({
+            marginTop: '0px',
+        },'slow');
+    
+
+});*/
+
+
